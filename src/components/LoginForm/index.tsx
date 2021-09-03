56bd3f58a58eee9cbe56dad892acf49f3482606a
fe-jcorreia <fernando.correia@taqtile.com.br>
2021-09-03 09:49:18 -0300

    separated styled components

--- src/components/LoginForm/index.tsx
+++ src/components/LoginForm/index.tsx
@@ -1,19 +1,19 @@
-import { Container } from './style';
+import { ButtonLogin, Container, FormLogin, InputLogin } from './style';
 
 export const LoginForm: React.FC = () => {
   return (
     <Container>
       <h1>Bem-vindo(a) à Taqtile!</h1>
 
-      <form>
+      <FormLogin>
         <label htmlFor='login-form-email'>E-mail</label>
-        <input id='login-form-email' type='text' name='E-mail' />
+        <InputLogin id='login-form-email' type='text' name='E-mail' />
 
         <label htmlFor='login-form-password'>Password</label>
-        <input id='login-form-password' type='password' name='Password' />
+        <InputLogin id='login-form-password' type='password' name='Password' />
 
-        <button type='submit'>Entrar</button>
-      </form>
+        <ButtonLogin type='submit'>Entrar</ButtonLogin>
+      </FormLogin>
     </Container>
   );
 }
