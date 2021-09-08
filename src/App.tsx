1e2ccce98d3c9fed8bfefa9d09ade12c2759d517
fe-jcorreia <fernando.correia@taqtile.com.br>
2021-09-08 16:44:40 -0300

    routing on login implemented + spinner on loading + auth-context created

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
+import { Route, Router, Switch } from 'react-router';
 import { LoginForm } from './components/LoginForm';
+import { createBrowserHistory } from 'history';
+import { Dashboard } from './components/Dashboard';
+
+const history = createBrowserHistory();
 
 function App(): JSX.Element {
-  return <LoginForm />;
+  return (
+    <Router history={history}>
+      <Switch>
+        <Route path='/' exact component={LoginForm} />
+        <Route path='/dashboard' component={Dashboard} />
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
