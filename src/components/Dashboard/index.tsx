bb21caaf853ef7e5ef310d045d7ec5b34b26f797
fe-jcorreia <fernando.correia@taqtile.com.br>
2021-09-09 14:05:21 -0300

    dashboard's userlist implemented

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,3 +1,20 @@
+import { Container, UserCard, UserList } from '../Dashboard/style';
+import data from '../../data.json';
+
 export const Dashboard: React.FC = () => {
-  return <h1>Dashboard</h1>;
+  return (
+    <Container>
+      <h1>User List</h1>
+      <UserList>
+        {data.map((user) => {
+          return (
+            <UserCard>
+              <h2>{user.name}</h2>
+              <p>{user.email}</p>
+            </UserCard>
+          );
+        })}
+      </UserList>
+    </Container>
+  );
 };
